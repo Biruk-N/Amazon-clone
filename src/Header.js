6f95debb2a5333e0b17549d0307302e8da624d80
biruk <birubu178@gmail.com>
2022-02-13 12:23:31 +0300

    header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,33 @@ import React from 'react'
 import './Header.css'
 function Header() {
   return (
-    <div className='header'>Header</div>
+    <div className = 'header'>
+        <img 
+            className="header
+            __logo" 
+            src="http://
+            pngimg.com/
+            uploads/amazon/
+            amazon_PNG11.png" 
+            
+        />
+
+        <div 
+        className="header__search"> 
+            <input
+            className="header__searchIn
+            put" type="text" />
+
+        </div>
+
+        <div 
+        className="header__nav"> 
+       
+
+        </div>
+        
+    </div>
+
   )
 }
 

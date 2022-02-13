5ca5861c98d0ead1ba1f1b1586e406cfa83650d3
biruk <birubu178@gmail.com>
2022-02-13 11:54:42 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
      //react-router needed for page routing
+   // because react by default doesn't include.
    
     <div className="App">
       <h1>Hello it's git test</h1>

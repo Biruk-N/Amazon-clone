70aef36949033cb0b7de9cfe3266ab458a0df26e
biruk <birubu178@gmail.com>
2022-02-13 11:20:52 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Clever Pro grammers, let's build the Amazon Store</h1>
+      <h1>Hello it's git test</h1>
     </div>
   );
 }

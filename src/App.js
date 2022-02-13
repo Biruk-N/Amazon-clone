eb21156dd4c3a55c06e50e450c94c47e8feb3967
biruk <birubu178@gmail.com>
2022-02-13 11:52:04 +0300

    router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    // BEM
+     //react-router needed for page routing
+   
     <div className="App">
       <h1>Hello it's git test</h1>
     </div>

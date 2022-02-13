97957a3386b6f530630f11051634d5594c327e99
biruk <birubu178@gmail.com>
2022-02-13 15:27:45 +0300

    header style

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 function Header() {
   return (
     <div className = 'header'>
         <img 
-            className="header
-            __logo" 
+            className="header__logo" 
             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
        />
 
@@ -50,7 +50,12 @@ function Header() {
                  </span>
            
             </div>
-
+            <div className="header__optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header__optionLineTwo 
+                header__basketCount">0</span>
+                
+            </div>  
         </div>
         
     </div>

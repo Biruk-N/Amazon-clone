8bfd22161f37293cf84c0136303efc73e632e549
biruk <birubu178@gmail.com>
2022-02-13 11:16:10 +0300

    react added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Clever Pro grammers, let's build the Amazon Store</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAhre39ZNnPHKPLz0gG1tV3xAID42LAX2M",
+    authDomain: "clone-9437c.firebaseapp.com",
+    projectId: "clone-9437c",
+    storageBucket: "clone-9437c.appspot.com",
+    messagingSenderId: "905103244967",
+    appId: "1:905103244967:web:c14c565545006973eca582",
+    measurementId: "G-K7WJ5MQNLQ"
+  };

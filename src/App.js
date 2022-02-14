aa7ea28b9fc4248f71e6bb6c9db537fb642c7b90
biruk <birubu178@gmail.com>
2022-02-14 21:47:28 +0300

    props pass for product

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import './App.css';
-import './Header'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter , Router, Routes, Route } from "react-router-dom";
+
 import Header from './Header';
 import Home from './Home';
 function App() {
   return (
-     //react-router needed for page routing
-   // because react by default doesn't include.
-  //  <Router></Router>
-      <div className="app">
-        
-        <Header />
-        <Home /> 
-      </div>
-    
-    
+    //react-router needed for page routing
+    // because react by default doesn't include.
+    // <Router>
+    //   <div className="app">
+    //    <Routes>
+    //     <Route  exact path="/">              
+    //         {/* Header path="Checkout"*/}
+    //         <Header />
+    //         <h1> I AM CHECKOUT</h1>
+    //         <Home />
+    //         {/* Home */}
+    //       </Route>
+
+    //       <Route path="/">              
+    //         {/* Header */}
+    //         <Header />
+    //         <Home />
+    //         {/* Home */}
+    //       </Route>
+ 
+    //    </Routes>
+
+    //   </div>
+    // </Router>
+    <div className="app">
+      <Header />
+       <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} >
+          
+          {/* <Route path="/he" element={} /> */}
+          
+          </Route>
+        </Routes>
+  </BrowserRouter>
+    </div>
   );
 }
 

eeeecbfb9d71b1bebf35f44e3d2ce5468343503c
biruk <birubu178@gmail.com>
2022-02-14 17:49:18 +0300

    props pass for product

--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import './Product.css'
-function Product() {
+function Product({title, image, price, rating }) {
   return (
     <div 
     className='product'>
         <div className="product__info">
-            <p>The Lean Startup</p>
+            <p>{title}</p>
             <p className='product__price'>
                 <small>$</small>
-                <strong>19.99</strong>
+                <strong>{price}</strong>
             </p>
             <div className='product__rating'>
-                <p>⭐️</p>
-                {/* <p>⭐️</p>
-                <p>⭐️</p>
-                <p>⭐️</p> */}
+                
+                {Array(rating)
+                    .fill()
+                    .map((_, i) => (
+                        <p>⭐️</p>
+                    ))}
             </div>
         </div>
         <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+        src={image}
         alt="" 
 
         />
+        
         <button>Add to Basket</button>
     </div>
   )

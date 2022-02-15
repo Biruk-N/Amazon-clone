2be32004a949dee22e375d37484401f34ef270cd
biruk <birubu178@gmail.com>
2022-02-15 17:52:20 +0300

    add to basket component

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -8,22 +8,22 @@ function Subtotal() {
      <CurrencyFormat
          renderText={(value) => (
             <>
-        <p>
-            Subtotal (0 items): <strong>0</strong>
-        </p>
-        <small className="subtotal gift">
-            <input type="checkbox" /> This order
-            contains a gift
-        </small>
+            <p>
+                Subtotal (0 items): <strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+                <input type="checkbox" /> This order
+                contains a gift
+            </small> 
         </>
         )}
         decimalScale={2}
-        value= {getBaskettotal(basket)}
+        value= {0}
         displayType={"text"}
         thousandSeparator={true}
-        prefix={"E"}
+        prefix={"$"}
     />
-
+    <button>Proceed to Checkout</button>
     </div>
   )
 }

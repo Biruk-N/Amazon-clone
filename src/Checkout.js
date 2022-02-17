c3d004d97ef4b7ff769e9f0c53777bb084653ad7
Biruk <86155266+Biruk-N@users.noreply.github.com>
2022-02-17 18:36:42 +0300

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Checkout.css'
+import Checkoutproduct from './Checkoutproduct';
+import { useStateValue } from './Stateprovider';
 import Subtotal from './Subtotal'
 
 function Checkout() {
+
+const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
         <div className="checkout__left">
@@ -20,6 +25,23 @@ function Checkout() {
             <h2>
                 The subtotal will go there
             </h2>    
+            {basket
+
+            .map(item => ( 
+
+            <Checkoutproduct
+                // id = {item.id}
+                title = {item.title}
+                image = {item.image}
+                price = {item.price}
+                rating = {item.rating}
+
+               
+            /> 
+            ))}
+
+
+
         </div>
 
     </div>

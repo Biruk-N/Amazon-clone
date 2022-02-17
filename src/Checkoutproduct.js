e672b5fda60b99379e0b1820ed0aefbf9b10519e
Biruk <86155266+Biruk-N@users.noreply.github.com>
2022-02-17 18:00:49 +0300

    Update Checkoutproduct.js

--- src/Checkoutproduct.js
+++ src/Checkoutproduct.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Checkoutproduct.css'
 
-function Checkoutproduct() {
+function Checkoutproduct(image, title, price, rating) {
   return (
     <div className="checkoutproduct">
-    
+      <img src={image} alt=''/>
     </div>
   )
 }

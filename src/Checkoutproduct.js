f1061c64463ddaa02fc789a35c78c1a1992c22ae
Biruk <86155266+Biruk-N@users.noreply.github.com>
2022-02-17 18:11:14 +0300

    pass by props

--- src/Checkoutproduct.js
+++ src/Checkoutproduct.js
@@ -4,7 +4,25 @@ import './Checkoutproduct.css'
 function Checkoutproduct(image, title, price, rating) {
   return (
     <div className="checkoutproduct">
-      <img src={image} alt=''/>
+      <img className='checkoutproduct__image' src={image} alt=''/>
+      
+      <div className='checkoutproduct__info'>
+        
+        <p className='checkproduct__title'>
+          {title}
+
+        </p>
+
+        <p className='checkproduct__price'>
+          <small>$</small>
+          <strong> {price} </strong>
+        </p>
+
+        <div className='checkoutproduct__rating'>
+          {rating}
+        </div>
+
+      </div>
     </div>
   )
 }
